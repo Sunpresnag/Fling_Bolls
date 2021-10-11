04b64a9de769c58bb63bfbafcb1fd5c8bda43302
Sunpresnag <savinov.nikolay.13@gmail.com>
2021-10-11 18:19:11 +0300

    commit 2

--- app/src/main/java/com/example/fling_bolls/MyDraw.java
+++ app/src/main/java/com/example/fling_bolls/MyDraw.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class MyDraw extends View {
@@ -12,14 +13,26 @@ public class MyDraw extends View {
     }
 
     @Override
-    protected void onAttachedToWindow() {
-        super.onAttachedToWindow();
-        //(float)(Math.random())
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+
     }
 
     Paint p = new Paint();
-    float x = 200f;
-    float y = 200f;
+    float[] x = new float[200];
+    float[] y = new float[200];
+
+    int n = 0;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        x[n] = event.getX();
+        y[n] = event.getY();
+        n++;
+        return false;
+
+    }
+
     float vx = 1;
     float vy = 2;
     boolean onEdge = false;
@@ -27,15 +40,18 @@ public class MyDraw extends View {
         super.onDraw(canvas);
         p.setStrokeWidth(5);
         p.setColor(Color.YELLOW);
-        canvas.drawCircle(x,y,50, p);
-        if(x > getWidth() || x < 0){
-            vx = -vx;
+        for(int i = 0; i < 200; i++){
+            canvas.drawCircle(x[i],y[i],50, p);
+            if(x[i] > getWidth() || x[i] < 0){
+                vx = -vx;
+            }
+            if(y[i] > getHeight() || y[i] < 0){
+                vy = -vy;
+            }
+            x[i] += vx;
+            y[i] += vy;
         }
-        if(y > getHeight() || y < 0){
-            vy = -vy;
-        }
-        x += vx;
-        y += vy;
+
         invalidate();
     }
 
